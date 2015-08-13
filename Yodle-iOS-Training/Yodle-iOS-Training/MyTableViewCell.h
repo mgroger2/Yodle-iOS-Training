03991f49181ab46dac10bbe05d60ba7326c712be
Mark Rogers <mark.rogers@yodle.com>
2015-08-13 11:19:44 -0400

    Added MyTableVewCell. Organized new filed into "View" & "Controller" Folders.

---
+++ Yodle-iOS-Training/Yodle-iOS-Training/MyTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewCell.h
+//  Yodle-iOS-Training
+//
+//  Created by Mark Rogers on 8/13/15.
+//  Copyright (c) 2015 Mark Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewCell : UITableViewCell
+
+@end
---
+++ Yodle-iOS-Training/Yodle-iOS-Training/MyTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.m
+//  Yodle-iOS-Training
+//
+//  Created by Mark Rogers on 8/13/15.
+//  Copyright (c) 2015 Mark Rogers. All rights reserved.
+//
+
+#import "MyTableViewCell.h"
+
+@implementation MyTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
