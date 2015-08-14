fabc2488462c36096e739b53a2855eaaf4741388
Mark Rogers <mark.rogers@yodle.com>
2015-08-14 12:52:47 -0400

    Installed CocoaPods & SDWebView Pod

---
+++ Yodle-iOS-Training/Pods/Target Support Files/Pods-Yodle-iOS-Training-SDWebImage/Pods-Yodle-iOS-Training-SDWebImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Yodle_iOS_Training_SDWebImage : NSObject
+@end
+@implementation PodsDummy_Pods_Yodle_iOS_Training_SDWebImage
+@end
---
+++ Yodle-iOS-Training/Pods/Target Support Files/Pods-Yodle-iOS-Training/Pods-Yodle-iOS-Training-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Yodle_iOS_Training : NSObject
+@end
+@implementation PodsDummy_Pods_Yodle_iOS_Training
+@end
